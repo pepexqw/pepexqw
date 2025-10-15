5fa72195fc1f77423728e50a02e1a16555c155a7
pepexqw <superampleev@gmail.com>
2025-10-15 11:47:13 +0300

    Add files via upload

---
+++ 404.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Custom404 = () => {
+  return (
+    <div>
+      <h1>Страница не найдена</h1>
+      <p>Извините, но вы попали на страницу, которая не существует.</p>
+    </div>
+  );
+};
+
+export default Custom404;
