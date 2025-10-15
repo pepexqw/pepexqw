5cee90a7d618831454e4d3356423dd7e08fd72fc
pepexqw <superampleev@gmail.com>
2025-10-15 11:57:34 +0300

    Delete 404.js

--- 404.js
+++
@@ -1,9 +0,0 @@
-export default function Custom404() {
-    return (
-        <div style={{ textAlign: 'center', margin: '50px' }}>
-            <h1>404 - Страница не найдена</h1>
-            <p>Извините, но данная страница не существует.</p>
-            <a href="/">Вернуться на главную</a>
-        </div>
-    );
-}
