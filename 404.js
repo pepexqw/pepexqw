9d8dd9384e55dbb6b77e6a19c4cb17e5cc50c637
pepexqw <superampleev@gmail.com>
2025-10-15 11:54:46 +0300

    Add files via upload

---
+++ 404.js
@@ -0,0 +1,9 @@
+export default function Custom404() {
+    return (
+        <div style={{ textAlign: 'center', margin: '50px' }}>
+            <h1>404 - Страница не найдена</h1>
+            <p>Извините, но данная страница не существует.</p>
+            <a href="/">Вернуться на главную</a>
+        </div>
+    );
+}
