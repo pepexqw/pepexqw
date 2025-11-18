1d2ef6d97c9d1e857f4991907faeff2e38775432
pepexqw <superampleev@gmail.com>
2025-11-18 09:12:41 +0300

    Delete teste.js

--- teste.js
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', fontSize: '48px' }}>
-      olo world
-    </div>
-  )
-}
