a73f956656fe9a449c6e6dd402d8d09c35a35bb8
pepexqw <superampleev@gmail.com>
2025-11-18 09:11:30 +0300

    Add files via upload

---
+++ teste.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', fontSize: '48px' }}>
+      olo world
+    </div>
+  )
+}
