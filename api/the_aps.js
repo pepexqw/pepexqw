e5e7008e8a3dafa3caa1311704d7d7ab18f5dbf3
pepexqw <superampleev@gmail.com>
2025-12-17 12:59:18 +0300

    Update the_aps.js

--- api/the_aps.js
+++ api/the_aps.js
@@ -5,14 +5,13 @@ export default function handler(req, res) {
         const data = req.body;
         console.log("Полученные данные:", data);
         
-        // Сохраняем сообщение
         messages.push(data.message);
         res.status(200).json({ status: 'success', received: data });
     } else if (req.method === 'GET') {
-        // Возвращаем все сообщения
         res.status(200).json({ messages });
     } else {
         res.setHeader('Allow', ['POST', 'GET']);
         res.status(405).end(`Метод ${req.method} не поддерживается`);
     }
 }
+
