73f894662404ae242cd61150b4871a66555131ce
pepexqw <superampleev@gmail.com>
2025-12-31 16:33:48 +0300

    Forum update

--- d.js
+++ d.js
@@ -6,7 +6,17 @@
       "text": "test",
       "time": "13:33",
       "date": "31.12",
-      "replies": [],
+      "replies": [
+        {
+          "id": "1aba4cf4",
+          "name": "Аноним",
+          "text": "tesrt",
+          "time": "13:33",
+          "date": "31.12",
+          "replies": [],
+          "parent": "16d93ed3"
+        }
+      ],
       "parent": null
     }
   ]
